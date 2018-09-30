a0fd240c6efa74456ab5896a43bd0291dfc016f2
Yury Chajkou <yury80@gmail.com>
2018-09-30 16:54:20 +0300

    Additional 5: Cheat solution removed, brackets calculator implemented.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var brArr = str.split('');
+    for (let c = 0; c < str.length; c++) {
+    for (var i = 0; i < brArr.length; i++) {
+      for (var j = 0; j < bracketsConfig.length; j++) {
+        if (brArr[i] == bracketsConfig[j][0] && brArr[i + 1] == bracketsConfig[j][1])
+          brArr.splice(i, 2)
+        if (brArr.length == 0) break
+      }
+    }
+  }
+  if (brArr.length == 0) {
+   return true;
+  }
+  else {
+   return false;
+  }
 }
